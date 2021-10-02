6ec3f5b1d377532706bf44e4a8eb8fe523ec1962
GelosoFederico <gelosofederico@gmail.com>
2021-10-02 01:53:49 -0300

    Mana affects speed

--- Player.gd
+++ Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
-export (int) var max_speed = 400
-export (int) var acceleration = 50
-export (int) var jump_speed = -600
+export (int) var max_speed_base = 400
+export (int) var acceleration_base = 50
+export (int) var jump_speed_base = -600
 export (int) var gravity = 1200
 export (int) var health = 100
+# mana_factor is how much max_speed, acceleratiorn and jump_speed are affected
+# by mana
+export (float) var mana_factor = 1
 
 export (int) var invincibility_time = 2
 export (float) var blinking_speed = 0.05
@@ -13,6 +16,9 @@ export (NodePath) var hud_path
 onready var hud = get_node(hud_path)
 
 var mana = 0
+var max_speed = max_speed_base
+var acceleration = acceleration_base
+var jump_speed = jump_speed_base
 
 var velocity = Vector2()
 
@@ -58,7 +64,15 @@ func _physics_process(delta):
 	if not current_state == PlayerStates.DEAD:
 		self.get_input()
 
+func affect_mana():
+	self.max_speed = max_speed_base + (self.mana * mana_factor) * (self.mana * mana_factor)
+	self.acceleration = acceleration_base + (self.mana * mana_factor) * (self.mana * mana_factor)
+	self.jump_speed = jump_speed_base - (self.mana * mana_factor) * (self.mana * mana_factor)
+	print(self.max_speed)
+	print(self.acceleration)
+	print(self.jump_speed)
 
 func _process(delta):
 	self.mana += delta
+	self.affect_mana()
 	hud.update_mana(self.mana)
