68f1ec32682aad9ef4b85c3703352e18c26755d5
Julián Absatz <julianabsatz@gmail.com>
2021-10-02 17:39:45 +0200

    plataformas desaparecen

--- FloorSegment.gd
+++ FloorSegment.gd
@@ -5,22 +5,26 @@ export (int) var expiration_speed = 1
 var expiration_started = false
 var expiration_time = expiration_time_base
 
+
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
+
 
 func _process(delta):
 	if expiration_started:
 		expiration_time -= delta * expiration_speed
-		$Sprite.modulate.r = (expiration_time_base - expiration_time)/expiration_time_base
-		$Sprite.modulate.g = (expiration_time_base - expiration_time)/expiration_time_base
-		$Sprite.modulate.b = (expiration_time_base - expiration_time)/expiration_time_base
+		$Sprite.modulate.r = (expiration_time_base - expiration_time) / expiration_time_base
+		$Sprite.modulate.g = (expiration_time_base - expiration_time) / expiration_time_base
+		$Sprite.modulate.b = (expiration_time_base - expiration_time) / expiration_time_base
 	if expiration_time < 0:
-		queue_free()
+		visible = false
+		collision_layer = 0
+		collision_mask = 0
 
 
 func start_expiration_timer():
 	expiration_started = true
-	
+
 
 func _on_Player_grab_body_entered(body):
 	self.start_expiration_timer()
