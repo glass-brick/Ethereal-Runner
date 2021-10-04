b3bab69185244812b8b39b5a5868723344f5dc0c
Julián Absatz <julianabsatz@gmail.com>
2021-10-04 21:05:53 +0200

    lightning effects

--- Lightning.gd
+++ Lightning.gd
@@ -7,12 +7,13 @@ func _ready():
 	$AnimatedSprite.play('default')
 
 
-func _on_AnimatedSprite_animation_finished():
+func _on_AudioStreamPlayer2D_finished():
 	queue_free()
 
 
 func _on_AnimatedSprite_frame_changed():
 	if $AnimatedSprite.frame == 10:
+		$AudioStreamPlayer2D.play()
 		$CPUParticles2D.restart()
 		var targets = get_overlapping_bodies()
 		for target in targets:
