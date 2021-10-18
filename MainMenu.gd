ec6f0eca9ac233f49dbc09d0e3b4df9405998227
Julián Absatz <julianabsatz@gmail.com>
2021-10-18 22:17:15 +0200

    sound options

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,14 +1,18 @@
 extends CanvasLayer
 
 var started = false
-
+enum MenuStates { FREE_CURSOR, SELECTED_OPTION }
+var current_state = MenuStates.FREE_CURSOR
+var option_path = []
+var focused_option_index = 0
 onready var cursor = $Path2D
 onready var current_menu = $MainMenu
-var focused_option = 0
+var current_option = null
 
 
 func _ready():
-	open_menu($MainMenu)
+	$SettingsMenu.visible = false
+	enter_menu($MainMenu)
 	$Path2D/PathFollow2D/AnimatedSprite.play('Idle')
 	var seconds = int(Globals.high_time) % 60
 	var minutes = int((Globals.high_time - seconds) / 60)
@@ -16,47 +20,87 @@ func _ready():
 
 
 func _process(delta):
-	var current_option = current_menu.get_child(focused_option)
+	if current_state == MenuStates.FREE_CURSOR:
+		process_free_cursor()
+	elif current_state == MenuStates.SELECTED_OPTION:
+		process_selected_option()
+	if started:
+		$Path2D/PathFollow2D.unit_offset += delta
+
+
+func process_selected_option():
+	if Input.is_action_just_pressed('ui_cancel') or Input.is_action_just_pressed('ui_accept'):
+		go_back()
+
+
+func process_free_cursor():
+	var focused_option = current_menu.get_child(focused_option_index)
 	cursor.position = (
 		current_menu.rect_position
-		+ current_option.rect_position
-		+ Vector2(-20, current_option.rect_size.y / 2)
+		+ focused_option.rect_position
+		+ Vector2(-20, focused_option.rect_size.y / 2)
 	)
 
 	if Input.is_action_just_pressed("ui_down"):
-		focused_option = (focused_option + 1) % current_menu.get_child_count()
+		focused_option_index = (focused_option_index + 1) % current_menu.get_child_count()
 	elif Input.is_action_just_pressed("ui_up"):
-		focused_option = (
-			(focused_option - 1)
-			if focused_option > 0
+		focused_option_index = (
+			(focused_option_index - 1)
+			if focused_option_index > 0
 			else (current_menu.get_child_count() - 1)
 		)
 
 	if not started and Input.is_action_just_pressed('ui_accept'):
 		select_option()
-	if started:
-		$Path2D/PathFollow2D.unit_offset += delta
 	if Input.is_action_just_pressed('ui_cancel'):
-		if current_menu != $MainMenu:
-			open_menu($MainMenu)
+		go_back()
 
 
 func select_option():
-	var option_text = current_menu.get_child(focused_option).text
-	match option_text:
+	print(option_path)
+	var focused_option = current_menu.get_child(focused_option_index)
+	var option_name = focused_option.name
+	match option_name:
 		"Tutorial":
 			start_tutorial()
-		"Start Game":
+		"StartGame":
 			start_game()
 		"Settings":
-			open_menu($SettingsMenu)
+			enter_menu($SettingsMenu)
+		"BGMVolume":
+			enter_slide_option(focused_option)
+		"SFXVolume":
+			enter_slide_option(focused_option)
+		"ControlRemapping":
+			pass
 
 
-func open_menu(menu):
+func enter_slide_option(slider):
+	current_state = MenuStates.SELECTED_OPTION
+	option_path.push_back(slider.name)
+	current_option = slider
+	current_option.selected = true
+
+
+func go_back():
+	if option_path.size() == 1:
+		return
+	current_state = MenuStates.FREE_CURSOR
+	option_path.pop_back()
+	if current_option:
+		current_option.selected = false
+		current_option = null
+	else:
+		var option_name = option_path[option_path.size() - 1]
+		enter_menu(get_node(option_name))
+
+
+func enter_menu(menu):
+	option_path.push_back(menu.name)
 	current_menu.visible = false
 	menu.visible = true
 	current_menu = menu
-	focused_option = 0
+	focused_option_index = 0
 
 
 func start_animation():
